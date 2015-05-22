ace0c72d231e3c6234e8f94283c2ab4d49880926
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2015-05-21 19:11:48 -0500

    working

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-IMAGE=mcandre/docker-redis:2.1
+IMAGE=mcandre/docker-redis:2.0
 
 all: run
 
--- site.pp
+++ site.pp
@@ -1,5 +1,5 @@
 class { 'redis':
-  redis_version_override => '2.1',
+  redis_version_override => '2.0',
   service_restart => false,
   service_enable => false,
   service_ensure => 'stopped'
